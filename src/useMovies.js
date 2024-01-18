434ce431d80fd845f0e8db934a882cd6306e33e6
Argentea-ya <argentea.ya@gmail.com>
2024-01-18 21:55:16 +0300

    mixed content fix

--- src/useMovies.js
+++ src/useMovies.js
@@ -18,7 +18,7 @@ export function useMovies(query) {
           setError('');
           const res = await fetch(
             `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal, referrerPolicy: 'unsafe_url' }
+            { signal: controller.signal, referrerPolicy: 'unsafe-url' }
           );
 
           if (!res.ok)
